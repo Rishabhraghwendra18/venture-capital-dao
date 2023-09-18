5ffaedaafbe8ec74c256a1695b2cd59c2ee7484a
rishabhraghwendra@18 <rishabhraghwendra2002@gmail.com>
2023-09-18 19:14:38 +0530

    made some css fixes and added proper description

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -5,19 +5,19 @@ import "./index.css";
 
 const features =[
     {
-        title:'$355M',
+        title:'$355M+',
         color:'#1FA5FF',
         description:'Total Funding Raised'
     },
     {
-        title:'$725M',
+        title:'$725M+',
         color:'#22DFAB',
         description:'Total Staked'
     },
     {
-        title:'461',
+        title:'400+',
         color:'#AC8AF8',
-        description:'Startups Funded'
+        description:'Projects Funded'
     },
 ]
 function HeroSection() {
@@ -28,14 +28,14 @@ function HeroSection() {
           padding: "1.8rem 0rem",
           display: "flex",
           gap: "10rem",
-          justifyContent: "center",
+          justifyContent: "space-between",
           alignItems:'center',
-          flex:0.6
+          flex:0.6,
         }}
       >
         <Box
           sx={{
-            //   width: "34rem",
+              maxWidth: "964px",
             display: "flex",
             flexDirection: "column",
             gap: "1.62rem",
@@ -56,8 +56,8 @@ function HeroSection() {
             variant="subtitle1"
             sx={{ fontSize: "1rem", color: "#2B2D42" }}
           >
-            DAO Maker creates growth technologies and funding frameworks for
-            startups, while simultaneously reducing risks for investors.
+            DAO Maker enables anyone to invest as little as $1 in Web3 projects. They can stake & vote on projects
+            they want to invest in. 
           </Typography>
 
           <Box sx={{ display: "flex", gap: "1rem", width: "100%" }}>
